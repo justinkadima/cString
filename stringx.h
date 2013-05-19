8137a66f515abb9a35afdda369a5b1f5bd73003f
justinkadima <justinkadima@yahoo.com>
2013-05-19 14:03:27 +0300

    check cpp

--- stringx.h
+++ stringx.h
@@ -1,9 +1,16 @@
 #ifndef STRINGX_H
 #define STRINGX_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 char* string_clone(const char* str);
 char* string_trim(const char* str,const char* keys);
 int string_countOccurence(const char* str,const char* token);
+
+#ifdef --cplusplus
+}
+#endif
 
 #endif
