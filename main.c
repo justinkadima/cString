db898c41877bbbd502acfe6fd446b422e3761165
justinkadima <justinkadima@gmail.com>
2014-02-24 18:47:14 +0200

    +template example

--- main.c
+++ main.c
@@ -7,111 +7,38 @@
 int main (int argc, const char * argv[])
 {
 
-    //test string_start & string_end
+    //simple template example
 
-    char* startTestString="This string starts with this";
-    if(string_startsWith(startTestString,"This"))
-    {
-        puts("string does start with This");
-    }
-    else
-    {
-        puts("test failed for startTestString");
-    }
-
-    char* endTestString="This string ends with that";
-    if(string_endsWith(endTestString,"that"))
-    {
-        puts("string does end with that");
-    }
-    else
-    {
-        puts("test failed for endTestString");
-    }
-
- 
-    
-    
-    const char* buf="  aici a fost aici se duce    ";
-    char* ret= string_trimMultiple(buf," ,i"); 
-    puts(ret);
-    free(ret);
+    const char* template="<template><p>Hello $name</p><div>Your value is: $val</div></template>";
 
 
-    const char* template="<html><body><h1>Title</h1><template><p>Hello $name</p><div>Your value is: $val</div></template></body></html>";
 
     TemplateParam par[2];
     par[0].name="$name";
-    par[0].value="John";
     par[1].name="$val";
-    par[1].value="London";
     
+
+    
+    //parse the text between <template> and </template> and replace the values 
+    
+    par[0].value="John";
+    par[1].value="London";
     char* pt=string_parseTemplate(template,"<template>","</template>",par,2);
-    puts(pt);
+    
     par[0].value="Dan";
     par[1].value="Germany";
 	char* spt=string_parseTemplate(template,"<template>","</template>",par,2);
-	puts(spt);
+	
 	
 	char* fpt=string_join(pt,spt);
+	
 	puts(fpt);
+	
 	
     free(pt);
     free(spt);
     free(fpt);
-    
-    char* one="the first";
-    char* two=" the last";
-    
-    char* sj=string_join(one, two);
-    
-    echo(sj);
-    
-    free(sj);
-    
-    char* lt={"Unu Dece"};
-    char* llt=string_toLower(lt);
-    echo(llt);
-    if(llt!=NULL)free(llt);
-
-
-    char* ut={"a small one"};
-    char* uret=string_toUpper(ut);
-    echo(uret);
-    free(uret);
-
-    char* aaa={"qwerty"};
-    char* aaa2=(char*)calloc(1,2*strlen(aaa)+1);
-    strcpy(aaa2,aaa);
-    puts(aaa2);
-    printf("SIZE: %lu \n",(unsigned long)strlen(aaa2));
-
-    free(aaa2);
-     
-    char* ddc=string_htmlEncode("/unu/doi?par=43&name=re done&val='32' ");
-    echo(ddc);
-    free(ddc); 
-    
-    
-    char inits[]="UN STRING";
-    const char* keyxor="pass";
-    
-    string_XORcypher(inits,keyxor);
-    
-    puts(inits);
-    
-   
-   //decript 
-     string_XORcypher(inits,keyxor);
-     puts(inits);
-        
-        
-     //url encode
-     
-     char url[]="http://foo bar/";
-     puts(string_urlEncode(url));
-        
-        
+ 
         
     return 0;
 }
